5d774a95120c20dc6114912811177e094c07d513
ryanbarillosofficial <129530399+ryanbarillosofficial@users.noreply.github.com>
2024-08-26 13:24:43 -0700

    Completed About Me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,37 @@
+/**
+ * About Me page
+ * 
+ * https://vercel.com/guides/loading-static-file-nextjs-api-route
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
+ * https://stackoverflow.com/questions/43174080/how-to-map-through-an-array-in-react-without-ids
+ */
 import React from 'react'
+import styles from "./styles.module.scss"
+import {promises as fs} from "fs"
+import about from "./about.json"
 
-const About = () => {
+
+const About = async() => {
+  /**
+   * Get text from text file
+   * 
+   * NOTE: In the future, there will be a shared palce to store texts in these pages:
+   * - Home
+   * - About
+   * 
+   * In the mean time, these will be "hard-coded"
+   */
+  const length: number = about.paragraph.length
   return (
-    <div>
+    <div className={styles.main}>
       <h1>About Me</h1>
-      <h2>Coming Soon</h2>
+      <div className={styles.block_text}>
+        {/* The headlin */}
+        <h2>{about.header}</h2>
+        {/* Each paragraph on the card */}
+        {about.paragraph.map((text, index) => {return(<p key={index}>{text}</p>)})} 
+      </div>
     </div>
   )
 }
